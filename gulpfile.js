6232fd7e4b9a84c1b1085d58fa04bb535f8c0636
Ratik Sharma <Ratik96@gmail.com>
2015-10-30 00:30:33 +0530

    Add plugin code, gulpfile, basic demo

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var gulp   = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+    minifyCss = require('gulp-minify-css');
+
+gulp.task("minifyScripts", function() {
+    return gulp.src("src/js/simplebox.js")
+        .pipe(gulp.dest('./demo/js'))
+        .pipe(uglify())
+        .pipe(rename('simplebox.min.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('compileSass', function() {
+    return gulp.src("src/css/style.scss")
+        .pipe(maps.init())
+        .pipe(sass())
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('./demo/css'))
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('outputImages', function() {
+    return gulp.src("src/img/cross.svg")
+        .pipe(gulp.dest('./demo/img'))
+        .pipe(gulp.dest('./dist/img'));
+});
+
+// gulp.task('watchSass', function() {
+//     gulp.watch('src/css/*.scss', ['compileSass']);
+// });
+
+gulp.task("build", ['minifyScripts', 'compileSass', 'outputImages']);
+
+gulp.task("default", ["build"]);
---
+++ src/js/simplebox.js
@@ -0,0 +1,55 @@
+'use strict';
+
+(function ($) {
+    $.fn.simplebox = function(options) {
+        var settings = $.extend({
+            fadeSpeed: 400,
+            closeImage: "img/cross.svg"
+        }, options);
+
+        // Helper Variables
+        var $body = $("body");
+        var $overlay = $('<div id="overlay"></div>');
+        var $cross = $('<img class="cross" src="' + settings.closeImage + '">');
+        var $image = $("<img class='slb'>");
+        var fadeSpeed = settings.fadeSpeed;
+
+        // When X is clicked or user clicks on the overlay div
+        // Hide lightbox
+        $overlay.click(function() {
+            $overlay.fadeOut(fadeSpeed);
+            $('.cross').addClass('cross-close');
+        });
+
+        $cross.click(function() {
+            $overlay.fadeOut(fadeSpeed);
+            $(this).addClass('cross-close');
+        });
+
+        return this.each(function() {
+            $("body").append($cross);
+            var $this = $(this);
+
+            // When the image is clicked
+            $this.click(function() {
+                console.log("Click");
+                var $this = $(this);
+                var imageSRC = $this.attr("src");
+                $image.attr("src", imageSRC);
+                $image.css("max-height", "80%");
+                $image.addClass('center');
+
+                $overlay.css('pointer-events', 'initial');
+
+                $cross.removeClass('cross-close');
+
+                $overlay.append($image);
+                $body.append($overlay);
+
+                // Show all the things!
+                $overlay.fadeIn(fadeSpeed);
+                $cross.show();
+            });
+        });
+    };
+}(jQuery));
